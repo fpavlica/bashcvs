c030552ec24ba48850b8645c5157dd75826be80c
ArchieRutherford <33519215+ArchieRutherford@users.noreply.github.com>
2018-10-03 17:09:59 +0100

    Moved things to functions

--- main.sh
+++ main.sh
@@ -1,5 +1,85 @@
 #! /bin/bash
 
+function addRepo() 
+{
+	echo
+	echo "Enter name of repository." 
+	read name
+	mkdir $name 
+}
+
+function delRepo() 
+{
+	echo 
+	echo "Which repository do you want to delete?"
+	counter=0
+	for index in $(ls)
+	do
+		let counter+=1
+		echo "$counter) $index"
+	done
+	read number
+	found=1
+	counter=0
+	for index in $(ls)
+	do
+		let counter+=1
+		if [ $counter == $number ]
+		then
+			rmdir $index
+		fi
+	done
+}
+
+function selRepo() 
+{
+	echo
+	echo "Which repository do you want to access?"
+	counter=0
+	for index in $(ls)
+	do
+		let counter+=1
+		echo "$counter) $index"
+	done
+	read number
+	counter=0
+	for index in $(ls)
+	do
+		let counter+=1
+		if [ $counter == $number ]
+		then
+			cd $index
+			fileMenu
+			cd ..
+		fi 
+	done
+}
+
+function fileMenu() 
+{
+	echo
+	if [ "$(ls -1|wc -l)" == 0 ]
+	then
+		echo "No files to show"
+	else
+		echo "Files:"	
+		ls -1 --color=auto
+	fi
+
+cat << FILE_MENU
+	
+     File Menu
+--------------------
+1) Log out file
+2) Log in file
+0) Quit
+FILE_MENU
+
+}
+
+
+
+
 cd repo
 
 cat << DOCUMENT
@@ -16,65 +96,15 @@ read option
 
 case $option in
 
-1)  
-echo
-echo "Enter name of repository." 
-read name
-mkdir $name 
-;;
+1) addRepo ;;
 
-2) 
-echo 
-echo "Which repository do you want to delete?"
-counter=0
-for index in $(ls)
-do
-	let counter+=1
-	echo "$counter) $index"
-done
-read number
-found=1
-counter=0
-for index in $(ls)
-do
-	let counter+=1
-	if [ $counter == $number ]
-	then
-		rmdir $index
-	fi
-done
-;;
+2) delRepo ;;
 
-3) 
-echo
-echo "Which repository do you want to access?"
-counter=0
-for index in $(ls)
-do
-	let counter+=1
-	echo "$counter) $index"
-done
-read number
-for index in $(ls)
-do
-	let counter+=1
-	if [ $counter == $number ]
-	then
-		cd $index
-		for $files in $(ls)
-		do
-			let number+=1
-			echo "$number) $files"
-		done
-		cd ..
-	fi 
-done
-;;
+3) selRepo ;;
 
 0)  
 echo 
 echo "Thanks for your time!"
-exit 0
 ;; 
 
 *) 
@@ -85,3 +115,4 @@ esac
 
 cd ..
 exit 0
+
